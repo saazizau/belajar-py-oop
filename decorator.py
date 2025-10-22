360834ebc3f2295b244b3fe2b88b2be704107581
Sabrina Aziz Aulia <saazizau@gmail.com>
2025-10-22 22:19:12 +0700

    melengkapi dekorator

--- decorator.py
+++ decorator.py
@@ -30,3 +30,20 @@ print(f"{bank_account1.no}, {bank_account1.balance}, {bank_account1.active}")
 
 bank_account2 = BankAccount.disabled("123324", 100000)
 print(f"{bank_account2.no}, {bank_account2.balance}, {bank_account2.active}")
+
+class Category:
+    __name = ""
+
+    @property
+    def name(self):
+        return self.__name
+    
+    @name.setter
+    def name(self, name):
+        if name == "":
+            raise ValueError("Nama tidak boleh kosong")
+        self.__name = name
+    
+category1 = Category()
+category1.name ="A"
+print(category1.__name)
